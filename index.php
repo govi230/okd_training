345ba4ef07ced3358dc5b844d1cdd6c574b2f931
govi230 <64520105+govi230@users.noreply.github.com>
2021-07-27 08:29:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <pre>
 <?php
 print "Welcome to RHOCP First Practical From CMD \n";
-print "Great Testing Result \n";
-print "What are you doing";
-print "Start Build From Command Line";
+print "New Testing";
 ?>
 </pre>
